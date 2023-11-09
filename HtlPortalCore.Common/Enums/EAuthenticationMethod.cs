84599afc0d18757304dd8235ea0fe20b43013ac0
SamFrieden <150334963+SamFrieden@users.noreply.github.com>
2023-11-09 11:33:39 +0100

    Add files via upload

---
+++ HtlPortalCore.Common/Enums/EAuthenticationMethod.cs
@@ -0,0 +1,8 @@
+﻿namespace HtlPortalCore.Common.Enums
+{
+    public enum EAuthenticationMethod
+    {
+        Phone = 1,
+        Email = 2
+    }
+}
---
+++ HtlPortalCore.Common/Helpers/AuthenticationHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace HtlPortalCore.Common.Helpers
+{
+    public static class AuthenticationHelpers
+    {
+        public static string CreateGuidString(string value)
+        {
+            using MD5 md5 = MD5.Create();
+            byte[] hash = md5.ComputeHash(Encoding.UTF8.GetBytes(value));
+            return new Guid(hash).ToString();
+        }
+    }
+}
